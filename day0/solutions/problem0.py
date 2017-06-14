9b0b1e02df4706462b81431358e3e8fc8174019f
Stephen S. Lee <Mr-Perfection@users.noreply.github.com>
2017-06-14 11:17:10 -0700

    Create problem0.py

--- day0/solutions/problem0.py
+++ day0/solutions/problem0.py
@@ -11,11 +11,13 @@ def FizzBuzz(n):
     for i in range(1, n+1):
         s = ''
 
-        if n % 3 == 0:
+        if i % 3 == 0:
             s += 'Fizz'
-        if n % 5 == 0:
+        if i % 5 == 0:
             s += 'Buzz'
         if s:
             print(s)
+        else:
+            print(i)
 
 FizzBuzz(100)
