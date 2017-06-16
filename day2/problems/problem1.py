44ada41e8c5ea87f6115baea6e23b53685d4e087
Sungsoo lee <sungsoolee0127@gmail.com>
2017-06-16 12:42:08 -0700

    day 2 in progress

---
+++ day2/problems/problem1.py
@@ -0,0 +1,16 @@
+'''
+Given an array of tickets(start, destination)
+[('SF', 'NY'), ('SJ', 'Chicago'), ('Chicago', 'LA'), ('NY', 'SJ')]
+Find the order of which the tickets were used by this person
+
+result:
+SF -> NY -> SJ -> Chicago -> LA
+
+
+Hint: ask me but try to look into the pattern. As you ask me more hints, it will lower your chance!
+
+Square
+'''
+
+def Solution(tickets: list):
+    
