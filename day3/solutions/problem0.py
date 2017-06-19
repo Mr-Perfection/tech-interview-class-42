5a5aeb67bb5331452f7278bc3f1e423ddf86100c
Sungsoo lee <sungsoolee0127@gmail.com>
2017-06-18 20:31:24 -0700

    problem0

--- day3/solutions/problem0.py
+++ day3/solutions/problem0.py
@@ -30,14 +30,35 @@ class Solution(object):
             alphas[ord(s[right])-ord('a')] -= 1
             # move right every time
             right += 1
-            
+
+
             if count == 0:
                 indices.append(left)
             
+            # if s[left:right].length == p.length
+            # then we need to fix left location
             if right - left == m:
-                if alphas[ord(s[left])-ord('a')] >= 0:
+                # check whether leftmost element
+                left_char_val = ord(s[left])-ord('a')
+                # increment count because this letter has been decremented previously
+                if alphas[left_char_val] >= 0:
                     count += 1
-                alphas[ord(s[left])-ord('a')] += 1
+                alphas[left_char_val] += 1
                 left += 1
-            
+
         return indices
+
+'''
+s = abcdecbabc
+p = cab
+
+alphas = [1,1,1,0....]
+right = 0
+left=0, right=0, count=3, alphas = [1,1,1,0....]
+left=0, right=0, count=2, alphas = [0,1,1,0....]
+left=0, right=1, count=2, alphas = [0,1,1,0....]
+left=0, right=2, count=1, alphas = [0,0,1,0....]
+left=0, right=3, count=0, alphas = [0,0,0,0....] -> [0]
+
+
+'''
